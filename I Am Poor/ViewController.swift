5d8eedde3664e3742bb5351a6fd47b5aab9d656a
David Gallacher <dr.gallacher@btinternet.com>
2020-04-30 16:54:41 +0100

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by David Gallacher on 30/04/2020.
+//  Copyright © 2020 David Gallacher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
